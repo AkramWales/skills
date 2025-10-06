64bb29f0034f16a47f863ea9b3f430693ffee0c1
AkramWales <walesakram@gmail.com>
2025-10-06 10:02:20 +0300

    updated homepage and styles
    updated homepage and styles

--- port.js
+++ port.js
@@ -5,13 +5,13 @@ const navLinks = document.querySelector(".nav-links");
 // Toggle menu on click
 menuToggle.addEventListener("click", () => {
     menuToggle.classList.toggle("active");
-    navLinks.classList.toggle("show");
+    navLinks.classList.toggle("active");
 });
 
 // Close menu when a link is clicked (for better UX)
 document.querySelectorAll(".nav-links a").forEach(link => {
     link.addEventListener("click", () => {
         menuToggle.classList.remove("active");
-        navLinks.classList.remove("show");
+        navLinks.classList.remove("active");
     });
 });
